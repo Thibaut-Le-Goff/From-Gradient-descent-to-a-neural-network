d33c3d53b14ed7626f1ab562e6630316a23ebabb
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2023-03-26 23:18:33 +0200

    preparing to deal with gradient descent

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,22 @@ fn main() {
     let inputs: Vec<Vec<f32>> = vec![vec![0.5], vec![2.3], vec![2.9]];
     //let inputs: Vec<Vec<f32>> = vec![vec![0.5], vec![2.3], vec![2.9], vec![2.3], vec![2.9], vec![2.3], vec![2.9]];
     //let inputs: Vec<Vec<f32>> = vec![vec![0.5, 0.5], vec![2.3, 0.5], vec![2.9, 0.5], vec![2.3, 0.5], vec![2.9, 0.5], vec![2.3, 0.5], vec![2.9, 0.5]];
-    //let observed_values: Vec<Vec<f32>> = vec![vec![1.4], vec![1.9], vec![3.2]];
-    let observed_values: Vec<Vec<f32>> = vec![vec![1.4], vec![1.9], vec![3.2], vec![1.9], vec![3.2], vec![1.9], vec![3.2]];
+
+
+    //let observed_values: Vec<Vec<f32>> = vec![vec![1.4], vec![1.9], vec![3.2], vec![1.9], vec![3.2], vec![1.9], vec![3.2]];
+    // Since propagation works with matrix multiplication, one per layers, and 
+    // backpropagation by gradient descent, one per neurons, 
+    // I can't process the datas the same way.
+    //
+    // in imputs = Vec<nb_propagations<nb_neurons_at_first_layer>>
+    // and
+    // in observed_values = Vec<nb_neurons_at_last_layer<nb_propagations>>
+    // but the datas will need to be put backward.
+
+    //let observed_values: Vec<Vec<f32>> = vec![vec![1.4, 1.9, 3.2, 1.4, 1.9, 3.2]];
+    //let observed_values: Vec<Vec<f32>> = vec![vec![1.4, 1.9, 3.2]];
+    //backward :
+    let observed_values: Vec<Vec<f32>> = vec![vec![3.2, 1.9, 1.4]];
 
     /* 
     let  datas = runst::DataSet {
@@ -24,8 +38,8 @@ fn main() {
     ///////////// Network settings ///////////////////
 
     let net = runst::Network {
-        network_struct : vec![1, 1, 2, 3, 4, 5, 6],
-        //network_struct : vec![1, 2],
+        //network_struct : vec![1, 1, 2, 3, 4, 5, 6],
+        network_struct : vec![1, 1],
         distrib : String::from("he_normal_dis"),
     
         hidden_activ_fun : String::from("none"),
@@ -42,48 +56,24 @@ fn main() {
 
     ////////////////////// PROPAGATION ////////////////////////////////////
     
-    let network_predictions: Vec<Vec<f32>> = runst::propagation::propagation(&net, &inputs ,&weights, &bias);
-    //let network_predictions: Vec<f32> = runst::propagation::propagation(&net, &inputs ,&weights, &bias);
+    //let network_predictions: Vec<Vec<f32>> = runst::propagation::propagation(&net, &inputs ,&weights, &bias);
+    let network_predictions: Vec<f32> = runst::propagation::propagation(&net, &inputs ,&weights, &bias);
     
     ////////////////////// BACK-PROPAGATION ////////////////////////////////////
 
     //let (mut trained_weights, mut trained_bias): (Vec<Vec<f32>>, Vec<Vec<f32>>) = runst::back_prop::back_prop(&net, &observed_values, &network_predictions ,&weights, &bias);
 
-    ///////////////////// MONTRE LES DONNÉES À L'ENVERS ////////////////////
-    println!("\nLes données en brut (à l'endroit) :");
-    for i in 0..network_predictions.len() {
-        println!("{:?}", network_predictions[i]);
-    }
-
-    println!("\n\nA l'envers :");
-
-    /*
-    for i in 0..weights.len() {
-        // for every layers of weigths
-        println!("\nDans la couche de poids numéro {} :", weights.len() - i);
-
-        let mut layers_counter: usize = weights.len() - i -1;
-
-        for y in 0..inputs.len() {
-            // for every propagations
-            println!("Les données de la propagation numéro {} sont :", y + 1);
-            //println!("lors de la propagation numéro {:?}", network_predictions[layers_counter]);
-
-            let vec_layer: &Vec<f32> = &network_predictions[layers_counter];
-
-            for j in 0..vec_layer.len() {
-                println!("{}", vec_layer[j]);
-            }
-            
-            layers_counter += weights.len();
-        }
-    }
-    */
-
-
-    let network_predictions_concat: &Vec<f32> = &network_predictions.concat();
-
+    let mut test: usize = runst::back_prop::back_prop(&net, &observed_values, &network_predictions ,&weights, &bias);
     
+    ///////////////////// MONTRE LES DONNÉES À L'ENVERS ////////////////////
+    //println!("\nLes données en brut (à l'endroit) :");
+    //println!("{:?}", network_predictions);
+
+    //println!("\n\nA l'envers :");
+
+    //let network_predictions_concat: &Vec<f32> = &network_predictions.concat();
+    
+    /*
     // This block calculate the sum of neurons in the network without the input layer
     let mut number_neurons: usize = 0;
     for i in 0..weights.len() {
@@ -92,19 +82,6 @@ fn main() {
         // for every layers of weights
         number_neurons += net.network_struct[i + 1];
     }
-
-    // This block order the datas, predictions, by neurons and by layers
-    /*
-    for i in 0..number_neurons {
-        // for every neurons of the network
-        let mut vec_neuron: Vec<f32> = Vec::new();
-
-        for y in 0..observed_values.len() {
-            // for every propagation
-            vec_neuron.push(network_predictions_concat[i + y * number_neurons]);
-        }
-    }
-    */
 
     let mut vec_predictions_neurons: Vec<Vec<f32>> = Vec::new();
 
@@ -116,9 +93,9 @@ fn main() {
         for y in 0..inputs.len() {
             // for every propagation
             let test: usize = i + y * number_neurons;
-            let test_reverse: usize = network_predictions_concat.len() - 1 - i - y * number_neurons;
+            let test_reverse: usize = network_predictions.len() - 1 - i - y * number_neurons;
 
-            vec_predictions_neuron.push(network_predictions_concat[test_reverse]);
+            vec_predictions_neuron.push(network_predictions[test_reverse]);
         }
 
         vec_predictions_neurons.push(vec_predictions_neuron)
@@ -138,4 +115,5 @@ fn main() {
 
         counter += net.network_struct[net.network_struct.len() - 1 - i];
     }
+    */
 }
